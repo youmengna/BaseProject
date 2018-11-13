50c0094e96139bbf06d81e73b72d55b67dc28ae6
cuibohan <cuibohan@corp.netease.com>
2018-11-13 11:09:37 +0800

    test code

--- testapp/src/main/java/com/xc/testapp/MainActivity.kt
+++ testapp/src/main/java/com/xc/testapp/MainActivity.kt
@@ -2,11 +2,17 @@ package com.xc.testapp
 
 import android.app.Activity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.xc.baseproject.BaseActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : Activity() {
+class MainActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        button.visibility = View.INVISIBLE
+        button.setOnClickListener { Toast.makeText(getContext(), "111", Toast.LENGTH_SHORT).show() }
     }
 }
